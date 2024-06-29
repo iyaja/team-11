952bc5851de7a7e1f94f33fd3f1b935965d0f7ee
Katrina Lui <katrina@descript.com>
2024-06-28 22:26:28 -0700

    Set up dnd and update styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
+import React from 'react';
 import './App.css'
 
 function App() {
+  const [isDragOver, setDragOver] = React.useState<boolean>(false);
+
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }
+
+  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setDragOver(false);
+  }
+
   return (
     <>
-      <p>Drop your document here</p>
-      <div className="dropzone">
-        
+      <span className="header">Upload your environmental impact statements here for quick assessment and insights</span>
+      <div className={`dropzone${isDragOver ? ' drag' : ''}`} onDragEnter={() => setDragOver(true)} onDragLeave={() => setDragOver(false)} onDragOver={onDragOver} onDrop={onDrop}>
+        Drop your document here
       </div>
     </>
   )
