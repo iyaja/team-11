5a5e1a92d8997cc8c0d49fbf5f6042ff94650e19
Katrina Lui <katrina@descript.com>
2024-06-28 21:09:23 -0700

    Initial react setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Drop your document here</p>
+      <div className="dropzone">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
