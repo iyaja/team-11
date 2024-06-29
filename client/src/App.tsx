3adeacbf76c26ab3e484b620a7e9c9137d2c200c
Katrina Lui <katrina@descript.com>
2024-06-29 08:05:34 -0700

    Add click to browse

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,21 +8,13 @@ import sampleJson from '../../server/sample_comments_3.json';
 import './App.css';
 
 function App() {
+  const inputRef = React.useRef<HTMLInputElement>(null);
   const [isDragOver, setDragOver] = React.useState<boolean>(false);
   const [isProcessing, setIsProcessing] = React.useState<boolean>(false);
   const [result, setResult] = React.useState<Result | undefined>(undefined);
 
-  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  }
-
-  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setIsProcessing(true);
-    setDragOver(false);
-    setTimeout(() => {
-      setIsProcessing(false);
+  const onSubmit = () => {
+    setIsProcessing(false);
       const sampleResult = sampleJson as Result;
       // Sort comments in descending order so that we can replace the quotes in reverse order to avoid changing the text indices
       const comments = sampleResult.comments.slice().sort((a, b) => b.quote.start - a.quote.start);
@@ -38,8 +30,38 @@ function App() {
       // Sort comments in ascending order of start index when displaying
       sampleResult.comments.sort((a, b) => a.quote.start - b.quote.start);
       setResult(sampleResult);
+  }
+
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }
+
+  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsProcessing(true);
+    setDragOver(false);
+    setTimeout(() => {
+      onSubmit();
     }, 2000);
   }
+
+  const onDropzoneClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    inputRef.current?.click();
+  }
+
+  const onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) {
+      return;
+    }
+    setIsProcessing(true);
+    setTimeout(() => {
+      onSubmit();
+    }, 2000);
+  }
+
 
   if (result) {
     return <Assessment result={result} />;
@@ -52,10 +74,17 @@ function App() {
       <span className="subheader">Upload statements for instant insights.</span>
       {isProcessing 
         ? <div className="processing"><ProcessingAnimation/></div>
-        : <div className={`dropzone${isDragOver ? ' drag' : ''}`} onDragEnter={() => setDragOver(true)} onDragLeave={() => setDragOver(false)} onDragOver={onDragOver} onDrop={onDrop}>
+        : <div className={`dropzone${isDragOver ? ' drag' : ''}`} onClick={onDropzoneClick} onDragEnter={() => setDragOver(true)} onDragLeave={() => setDragOver(false)} onDragOver={onDragOver} onDrop={onDrop}>
+          <input
+            ref={inputRef}
+            type="file"
+            style={{ display: 'none' }}
+            accept=".doc,.docx,.pdf,.txt"
+            onChange={onFileInputChange}
+        />
           <DocumentIcon />
           <div className="drop-main">Upload a document</div>
-          <div className="drop-sub">Drag & drop a file here</div>
+          <div className="drop-sub">Drag & drop a file here<br/>or click to browse</div>
         </div>
       }
     </>
